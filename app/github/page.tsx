236c704e213a37e31058d5c70bcd9859466048eb
indra cahya <indracahyae@gmail.com>
2025-03-17 00:52:12 +0700

    github exp page

--- app/github/page.tsx
+++ app/github/page.tsx
@@ -2,7 +2,7 @@ const GithubPage = () => {
   return (
     <div>
       main page
-      {/* search form */}
+      {/* search form by username*/}
       {/* users (first 5 users): card list*/}
       {/* repositories: modal > card repo list */}
     </div>
