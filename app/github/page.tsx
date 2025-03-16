0384d74b5708c59f3cfad0be9f4c3d18fcd3a5d7
indra cahya <indracahyae@gmail.com>
2025-03-16 14:04:18 +0700

    prepare

---
+++ app/github/page.tsx
@@ -0,0 +1,11 @@
+const GithubPage = () => {
+  return (
+    <div>
+      main page
+      {/* search form */}
+      {/* users (first 5 users): card list*/}
+      {/* repositories: modal > card repo list */}
+    </div>
+  );
+};
+export default GithubPage;
