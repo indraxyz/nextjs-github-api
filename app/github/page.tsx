4909e1837e33091d88eeb81c08722e487f64035c
indracahyae <indracahyae@gmail.com>
2025-03-18 18:35:13 +0700

    explorer

--- app/github/page.tsx
+++ app/github/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const GithubPage = () => {
+  useEffect(() => {
+    async function _fetchUsers() {
+      await fetch("https://api.github.com/search/users?q=indrax&per_page=5")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+
+      // set awaiting tasks
+      // console.log("run2");
+    }
+
+    _fetchUsers();
+    // console.log("run1");
+  }, []);
+
+  // _searchUsers
+
+  // _searchUserRepositories
+
   return (
     <div>
       main page
       {/* search form by username*/}
       {/* users (first 5 users): card list*/}
-      {/* repositories: modal > card repo list */}
+      {/* repositories: modal > repo list (listbox) */}
     </div>
   );
 };
