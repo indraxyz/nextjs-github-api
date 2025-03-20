8b552872eb2e0c28f630954a3705cf04474d797a
indracahyae <indracahyae@gmail.com>
2025-03-20 19:31:55 +0700

    list users

--- app/github/page.tsx
+++ app/github/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Input, Button, addToast } from "@heroui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Input, addToast, Card, CardHeader, CardFooter } from "@heroui/react";
+import {
+  RiSearchLine,
+  RiUser6Fill,
+  RiGithubFill,
+  RiGitRepositoryLine,
+} from "react-icons/ri";
+
+type colorToast =
+  | "secondary"
+  | "default"
+  | "foreground"
+  | "primary"
+  | "success"
+  | "warning"
+  | "danger"
+  | undefined;
 
 const GithubPage = () => {
   const [search, setSearch] = useState("");
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    _searchUsers("enter");
+    // _searchUsers("enter");
     _searchUserRepositories();
     _detailUserProfile();
   }, []);
@@ -19,23 +35,25 @@ const GithubPage = () => {
         .then((res) => res.json())
         .then((data) => {
           // USERS default sort ascending
+          setUsers(data.items);
           console.log(data.items);
-          addToast({
-            title: "Search success ",
-            description: "desc ...",
-            color: "secondary",
-          });
+          _toast("Users Search", "Search Success", "secondary");
         });
     } else {
       // notif
       if (k.toLowerCase() == "enter") {
-        addToast({
-          title: "Input tidak sesuai",
-          description: "rules dari sini ...",
-          color: "warning",
-        });
+        _toast("invalid", "minimal 3 character", "warning");
       }
     }
+  };
+
+  const _toast = (title: string, description: string, color: colorToast) => {
+    addToast({
+      title,
+      description,
+      color,
+      timeout: 2000,
+    });
   };
 
   const _searchUserRepositories = async () => {
@@ -59,7 +77,7 @@ const GithubPage = () => {
   return (
     <div className="p-4">
       {/* search form by username*/}
-      <div className="w-1/4">
+      <div className="w-full sm:w-1/4">
         <Input
           label="Search Github Users"
           labelPlacement="outside"
@@ -74,6 +92,40 @@ const GithubPage = () => {
         />
       </div>
       {/* users (first 5 users): card list*/}
+      <div className="flex flex-col max-sm:space-y-2 sm:flex-row sm:space-x-2 mt-6">
+        <Card className="w-full sm:w-1/3">
+          <CardHeader>
+            <RiUser6Fill className="text-4xl mr-2" />
+            Username
+          </CardHeader>
+          <CardFooter className="space-x-2">
+            <RiGithubFill className="text-xl" />
+            <RiGitRepositoryLine className="text-xl" />
+          </CardFooter>
+          {/* location, blog, bio */}
+        </Card>
+        <Card className="w-full sm:w-1/3">
+          <CardHeader>
+            <RiUser6Fill className="text-4xl mr-2" />
+            Username
+          </CardHeader>
+          <CardFooter className="space-x-2">
+            <RiGithubFill className="text-xl" />
+            <RiGitRepositoryLine className="text-xl" />
+          </CardFooter>
+        </Card>
+        <Card className="w-full sm:w-1/3">
+          <CardHeader>
+            <RiUser6Fill className="text-4xl mr-2" />
+            Username
+          </CardHeader>
+          <CardFooter className="space-x-2">
+            <RiGithubFill className="text-xl" />
+            <RiGitRepositoryLine className="text-xl" />
+          </CardFooter>
+        </Card>
+      </div>
+
       {/* repositories: modal > repo list (listbox) */}
     </div>
   );
